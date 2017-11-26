0fe95c65c0be312b1af46d74a9eb39979eea5cfd
Glumac <necaglumicic@gmail.com>
2017-11-26 21:30:39 +0100

    Added some animations to the home page
    Added some animations to the home page and a gif.

--- Home Page/JSCRIPT.js
+++ Home Page/JSCRIPT.js
@@ -11,3 +11,39 @@ $(window).scroll(function()
         $(".navbar").removeClass("navbar-scrolled");
     }
 });
+
+$(window).scroll(function()
+{
+    if($(this).scrollTop() > 400)
+    {
+        $(".beginnerpre").addClass("flip");
+        $(".intermidiatepre").addClass("flip2");
+        $(".advancedpre").addClass("flip3");
+        $(".beginnerpre").removeClass("opacity");
+        $(".beginnerpre").addClass("opacity2");
+        $(".intermidiatepre").removeClass("opacity");
+        $(".intermidiatepre").addClass("opacity2");
+        $(".advancedpre").removeClass("opacity");
+        $(".advancedpre").addClass("opacity2");
+    }             
+});
+
+$(window).scroll(function()
+{
+    if($(this).scrollTop() > 1200)
+    {
+        $(".tutorialsspan").addClass("flip");
+        $(".tutorialsspan").removeClass("opacity");
+        $(".tutorialsspan").addClass("opacity2");
+    }             
+});
+
+$(window).scroll(function()
+{
+    if($(this).scrollTop() > 1270)
+    {
+        $(".clickherespan").addClass("fadeIn");
+        $(".clickherespan").removeClass("opacity");
+        $(".clickherespan").addClass("opacity2");
+    }             
+});
