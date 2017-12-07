7f7889ecdd7236ca2fe7af7858875f2bb5c8f38e
Glumac <necaglumicic@gmail.com>
2017-12-07 20:55:54 +0100

    Finished The Log In Page

--- Log In/JSCRIPT.js
+++ Log In/JSCRIPT.js
@@ -1,43 +1,46 @@
-$("#newUser").click(function()
+$(() =>
 {
-  $("h1").text("Registration");
-  $(".logo").css(
-  {
-    "width":"150px",
-    "height":"150px",
-    "top":"-10px"
-  });
-  $("#login-form").fadeOut(200);
-  $("#registration-form").delay(300).fadeIn(500);
-  $(".other-options").fadeOut(200);
-});
+    $("#newUser").click(function()
+    {
+        $("h1").text("Registration");
+        $(".logo").css(
+        {
+            "width":"170px",
+            "height":"170px",
+            "top":"10px"
+        });
+        $("#login-form").fadeOut(200);
+        $("#registration-form").delay(300).fadeIn(500);
+        $(".other-options").fadeOut(200);
+    });
 
-$("#signup-btn,#getpass-btn").click(function()
-{
-  $("h1").text("Log in");
-  $(".logo").css(
-  {
-    "width":"150px",
-    "height":"150px",
-    "top":"30px"
-  });
+    $("#signup-btn,#getpass-btn").click(function()
+    {
+        $("h1").text("Log in");
+        $(".logo").css(
+        {
+            "width":"190px",
+            "height":"190px",
+            "top":"30px"
+        });
 
-  $("#registration-form,#fpass-form").fadeOut(200);
-  $("#login-form").delay(300).fadeIn(500);
-  $(".other-options").fadeIn(300);
-});
+        $("#registration-form,#fpass-form").fadeOut(200);
+        $("#login-form").delay(300).fadeIn(500);
+        $(".other-options").fadeIn(300);
+    });
 
-$("#fPass").click(function()
-{
-  $("h1").text("Forgot password?");
-  $(".logo").css(
-  {
-    "width":"230px",
-    "height":"230px",
-    "top":"15px";
-  });
-
-  $("#login-form").fadeOut(200);
-  $("#fpass-form").delay(300).fadeIn(500);
-  $(".other-options").fadeOut(200);
+    $("#fPass").click(function()
+    {
+        $("h1").text("Forgotten password");
+    
+        $(".logo").css(
+        {
+            "width":"240px",
+            "height":"240px",
+            "top":"35px"
+        });
+        $("#login-form").fadeOut(200);
+        $("#fpass-form").delay(300).fadeIn(500);
+        $(".other-options").fadeOut(200);
+    });
 });
