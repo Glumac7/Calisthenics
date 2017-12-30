e0fe29c456cacd3b9e1c5152357c878f846979d4
Glumac <necaglumicic@gmail.com>
2017-12-30 12:54:11 +0100

    Finished the BEGINNER page, just need to add some animations

--- Programs/Beginner/Beginner_JSCRIPT.js
+++ Programs/Beginner/Beginner_JSCRIPT.js
@@ -11,3 +11,17 @@ $(document).ready(function()
 		$('.parallax').css('background-position','center -'+((scrolledY*.5))+'px');
    	}
 });
+
+$(document).ready(function()
+{
+	$(window).bind('scroll',function(e)
+    {
+   		parallaxScroll();
+   	});
+ 
+   	function parallaxScroll()
+    {
+   		var scrolledY = $(window).scrollTop();
+		$('.parallax2').css('background-position','center -'+((scrolledY*.3))+'px');
+   	}
+});
